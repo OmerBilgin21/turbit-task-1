a43a7bf13aac96fabc2ee5c68f25cfa966730a20
OmerBilgin21 <omrfrkblgn21@gmail.com>
2024-07-07 20:07:19 +0200

    feat: schemas for ObjectId and items from mongo

---
+++ src/schemas/__init__.py
@@ -0,0 +1,12 @@
+from .comments import Comment
+from .people import Address, Company, Geo, People
+from .posts import Post
+
+__all__ = [
+	"Address",
+	"Post",
+	"Company",
+	"Comment",
+	"Geo",
+	"People",
+]
